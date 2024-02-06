af8fa9187ae6f6cc58d9ac7bfdc0d9b5ee5d8df7
Chao Meng <cmeng036@gmail.com>
2024-02-05 21:36:59 -0500

    added seeds and migrations files

---
+++ testdb.js
@@ -0,0 +1,14 @@
+const knexConfig = require("./knexfile").development;
+const knex = require("knex")(knexConfig);
+
+knex
+  .raw("SELECT 1+1 AS result")
+  .then((queryResponse) => {
+    console.log("Database connection test successful:", queryResponse);
+  })
+  .catch((err) => {
+    console.error("Database connection test failed:", err);
+  })
+  .finally(() => {
+    knex.destroy();
+  });

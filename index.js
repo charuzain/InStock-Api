68d2a7b8593984bc4a7151358779a0e4f6b125a8
charuzain <charuabhizain@gmail.com>
2024-02-05 19:11:58 -0500

    Install & Require Dependencies

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8081;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});

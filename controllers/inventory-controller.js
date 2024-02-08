dc6d9bf895774a56c1135c315c82436b28de58d8
charuzain <charuabhizain@gmail.com>
2024-02-07 20:15:26 -0500

    Add basic skeleton for all inventory routes

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,13 +1,47 @@
 const knex = require('knex')(require('../knexfile'));
 
 const getInventory = async (req, res) => {
+  console.log('ji');
+  try {
+    const data = await knex('inventories');
+    console.log(data);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(`Error retriving warehouse data`);
+  }
+};
+
+const getInventoryById = async (req, res) => {
   try {
     
   } catch (error) {
-   
+    
   }
+};
+
+const addInventoryItem = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+const editInventoryItem = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const deleteInventoryItem = async (req, res) => {
+  try {
+  } catch (error) {}
 };
 
 module.exports = {
   getInventory,
+  getInventoryById,
+  addInventoryItem,
+  editInventoryItem,
+  deleteInventoryItem
 };

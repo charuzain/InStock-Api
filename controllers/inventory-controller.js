6c2db9b0b0e536e100df65c211e7b02bd6568cdb
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-02-08 13:42:03 -0500

    added get inventory by ID

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -22,7 +22,22 @@ const getInventory = async (req, res) => {
 // Get inventory by ID
 const getInventoryById = async (req, res) => {
   try {
-  } catch (error) {}
+    const inventory = await knex("inventories")
+      .where({ id: req.params.id })
+      .first();
+
+    if (!inventory) {
+      return res.status(404).json({
+        message: `Inventory with ID ${req.params.id} not found`,
+      });
+    }
+    res.status(200).json(inventory);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve inventory data for inventory with ID ${req.params.id}`,
+    });
+    console.error(error);
+  }
 };
 
 const addInventoryItem = async (req, res) => {
